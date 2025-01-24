ca9735fb4955754a6fe9cf269a155a15f5e7b62a
Simon Zimmermann <mail@simonzimmermann.de>
2025-01-24 18:06:07 +0100

    simplify Engine

--- seele/seeleCore/src/Engine.cpp
+++ seele/seeleCore/src/Engine.cpp
@@ -1,7 +1,5 @@
 #include "Engine.h"
-#include "Config.h"
 #include "IMemberParameterSet.h"
-#include "PitchShifter.h"
 #include "core/AudioBuffer.h"
 
 
@@ -21,9 +19,6 @@ namespace hidonash
 
     void Engine::process(core::IAudioBuffer& inputBuffer)
     {
-        int numSummonedMembers = 0;
-
-        // Temporary buffer to accumulate results
         core::AudioBuffer outputBuffer(inputBuffer.getNumChannels(), inputBuffer.getNumSamples());
         outputBuffer.fill(0.f);
 
@@ -31,28 +26,17 @@ namespace hidonash
         {
             if (memberParameterSet_.getSummonState(n))
             {
-                // Prepare the audio buffer for processing
                 audioBuffers_[n]->setSize(inputBuffer.getNumChannels(), inputBuffer.getNumSamples());
                 audioBuffers_[n]->copyFrom(inputBuffer);
-
-                // Set pitch shift ratio and process the buffer
                 pitchShifters_[n]->setPitchRatio(memberParameterSet_.getSanctity(n));
                 pitchShifters_[n]->process(*audioBuffers_[n]);
 
-                // Accumulate the processed buffer into the output buffer
                 outputBuffer.add(*audioBuffers_[n], inputBuffer.getNumSamples());
-
-                ++numSummonedMembers;
             }
         }
 
-        if (numSummonedMembers > 0)
-        {
-            // Normalize the accumulated output buffer
-            outputBuffer.multiply(1.f / static_cast<float>(numSummonedMembers), inputBuffer.getNumSamples());
-        }
+        outputBuffer.multiply(1.f / static_cast<float>(numMembers_), inputBuffer.getNumSamples());
 
-        // Copy the output buffer back to the input buffer
         inputBuffer.copyFrom(outputBuffer);
     }
 }
