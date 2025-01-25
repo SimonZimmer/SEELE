71d66ef872edc091998711c44b526cbe66f83bc4
Simon Zimmermann <mail@simonzimmermann.de>
2025-01-25 15:14:06 +0100

    reduce oversampling

--- seele/seeleCore/inc/seeleCore/Config.h
+++ seele/seeleCore/inc/seeleCore/Config.h
@@ -21,7 +21,7 @@ namespace hidonash
         {
             constexpr auto fftFrameSize = 2048;
             constexpr auto analysisSize = 2 * fftFrameSize;
-            constexpr auto oversamplingFactor = 16;
+            constexpr auto oversamplingFactor = 2;
             constexpr auto stepSize = fftFrameSize / config::constants::oversamplingFactor;
             constexpr auto expectedPhaseDifference = 2. * M_PI * (double)stepSize / (double)fftFrameSize;
             constexpr auto numMembers = 7;
