16fff64d097d5bccdb71dbf8bc29dad83a0490ba
Simon Zimmermann <mail@simonzimmermann.de>
2025-01-29 23:53:57 +0100

    add uiconstants

---
+++ seele/inc/UiConstants.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <juce_gui_basics/juce_gui_basics.h>
+
+namespace hidonash
+{
+    namespace uiconstants
+    {
+        const juce::Colour highlightColour {juce::Colours::white};
+        const juce::Colour baseColour {juce::Colours::red};
+    }
+}
